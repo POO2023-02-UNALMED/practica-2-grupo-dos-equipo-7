090fbb917094bf4c63cc687607b546cdecc11562
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-11-07 15:40:11 -0500

    Update Graficos.py

--- src/uiMain/Graficos.py
+++ src/uiMain/Graficos.py
@@ -110,84 +110,124 @@ class FieldFrame(tk.Frame):
 
 
 
+class VentanaInicial:
+    def __init__(self):
+        pass
 
-def genVentanaInicial():
-    
-    #Constantes?
+    def generar(self):
+        frame_grande = tk.Frame(App, bg="blue")
+        frame_grande.grid(row=0, column=0, sticky="nsew")
+        App.grid_rowconfigure(0, weight=1)
+        App.grid_columnconfigure(0, weight=1)
 
-    frame_grande = tk.Frame(App, bg="blue")
-    frame_grande.grid(row=0, column=0, sticky="nsew")
-    App.grid_rowconfigure(0, weight=1)
-    App.grid_columnconfigure(0, weight=1)
+        welcome_label = tk.Label(frame_grande, text="Inicio",fg="red")
+        welcome_label.grid(row=0,column=0,padx=5, pady=5,sticky="nw")
 
-    welcome_label = tk.Label(frame_grande, text="Inicio",fg="red")
-    welcome_label.grid(row=0,column=0,padx=5, pady=5,sticky="nw")
+        p1 = tk.Frame(frame_grande, bg="green", borderwidth=1, relief="solid")
+        p1.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        frame_grande.grid_rowconfigure(1, weight=1)
+        frame_grande.grid_columnconfigure(0, weight=1)
 
-    p1 = tk.Frame(frame_grande, bg="green", borderwidth=1, relief="solid")
-    p1.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
-    frame_grande.grid_rowconfigure(1, weight=1)
-    frame_grande.grid_columnconfigure(0, weight=1)
+        p2 = tk.Frame(frame_grande, bg="red", borderwidth=1, relief="solid")
+        p2.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
+        frame_grande.grid_rowconfigure(1, weight=1)
+        frame_grande.grid_columnconfigure(1, weight=1)
 
-    p2 = tk.Frame(frame_grande, bg="red", borderwidth=1, relief="solid")
-    p2.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
-    frame_grande.grid_rowconfigure(1, weight=1)
-    frame_grande.grid_columnconfigure(1, weight=1)
+        p3 = tk.Frame(p1, bg="yellow", borderwidth=1, relief="solid")
+        p3.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        p1.grid_rowconfigure(0, weight=1)
+        p1.grid_columnconfigure(0, weight=1)
 
-    p3 = tk.Frame(p1, bg="yellow", borderwidth=1, relief="solid")
-    p3.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
-    p1.grid_rowconfigure(0, weight=1)
-    p1.grid_columnconfigure(0, weight=1)
+        p4 = tk.Frame(p1, bg="orange", borderwidth=1, relief="solid")
+        p4.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        p1.grid_rowconfigure(1, weight=1)
+        p1.grid_columnconfigure(0, weight=1)
 
-    p4 = tk.Frame(p1, bg="orange", borderwidth=1, relief="solid")
-    p4.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
-    p1.grid_rowconfigure(1, weight=1)
-    p1.grid_columnconfigure(0, weight=1)
-    
-    p5 = tk.Frame(p2, bg="purple", borderwidth=1, relief="solid")
-    p5.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
-    p2.grid_rowconfigure(0, weight=1)
-    p2.grid_columnconfigure(0, weight=1)
-    
-    p6 = tk.Frame(p2, bg="pink", borderwidth=1, relief="solid")
-    p6.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
-    p2.grid_rowconfigure(1, weight=1)
-    p2.grid_columnconfigure(0, weight=1)
+        p5 = tk.Frame(p2, bg="purple", borderwidth=1, relief="solid")
+        p5.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        p2.grid_rowconfigure(0, weight=1)
+        p2.grid_columnconfigure(0, weight=1)
 
-    saludo = "Bienvenid@ al sistema de venta de vuelos"
-    saludo_label = tk.Label(p3,text=saludo)
-    saludo_label.grid(row=0,column=0,padx=5, pady=5)
+        p6 = tk.Frame(p2, bg="pink", borderwidth=1, relief="solid")
+        p6.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        p2.grid_rowconfigure(1, weight=1)
+        p2.grid_columnconfigure(0, weight=1)
+
+        saludo = "Bienvenid@ al sistema de venta de vuelos"
+        saludo_label = tk.Label(p3,text=saludo)
+        saludo_label.grid(row=0,column=0,padx=5, pady=5)
+        pass
+
+class MainMenu:
+    def __init__(self):
+        pass
+
+    def generar(self):
+
+        #Botones de arriba:
+        archivoButton = ""
+        procesosconsultasButton = ""
+        ayudaButton = ""
+
+        frame_grande = tk.Frame(App, bg="blue")
+        frame_grande.grid(row=0, column=0, sticky="nsew")
+        App.grid_rowconfigure(0, weight=1)
+        App.grid_columnconfigure(0, weight=1)
+
+        nombre_aplicacion = tk.Label(frame_grande, text="Nombre aplicacion",fg="red")
+        nombre_aplicacion.grid(row=0,column=0,padx=5, pady=5,sticky="nw")
+
+        marco = tk.Frame(frame_grande, bg="green", borderwidth=1, relief="solid")
+        marco.grid(row=1, column=0, sticky="nsew", padx=5, pady=10)
+        frame_grande.grid_rowconfigure(1, weight=1)
+        frame_grande.grid_columnconfigure(0, weight=1)
+
+        zona1 = tk.Frame(marco, bg="yellow", borderwidth=1, relief="solid")
+        zona1.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        marco.grid_rowconfigure(0, weight=1)
+        marco.grid_columnconfigure(0, weight=1)
+
+        zona2 = tk.Frame(marco, bg="orange", borderwidth=1, relief="solid")
+        zona2.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        marco.grid_rowconfigure(1, weight=1)
+        marco.grid_columnconfigure(0, weight=1)
+
+        self.marco = marco
+        pass
 
 
-    pass
+class ProcesoConsulta:
+    def __init__(self, zona):
+        self.zona = zona
+        self.nombre = "Nombre Proceso"
+        self.descripcion = "Descripcion"
+        
+        self.criterios = []
+        
+        pass
 
-def genMainMenu():
-    
-    frame_grande = tk.Frame(App, bg="blue")
-    frame_grande.grid(row=0, column=0, sticky="nsew")
-    App.grid_rowconfigure(0, weight=1)
-    App.grid_columnconfigure(0, weight=1)
+    def generar(self):
+        zona1 = tk.Frame(self.zona, bg="yellow", borderwidth=1, relief="solid")
+        zona1.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        marco.grid_rowconfigure(0, weight=1)
+        marco.grid_columnconfigure(0, weight=1)
 
-    nombre_aplicacion = tk.Label(frame_grande, text="Nombre aplicacion",fg="red")
-    nombre_aplicacion.grid(row=0,column=0,padx=5, pady=5,sticky="nw")
+        zona2 = tk.Frame(self.zona, bg="orange", borderwidth=1, relief="solid")
+        zona2.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        marco.grid_rowconfigure(1, weight=1)
+        marco.grid_columnconfigure(0, weight=1)
 
-    marco = tk.Frame(frame_grande, bg="green", borderwidth=1, relief="solid")
-    marco.grid(row=1, column=0, sticky="nsew", padx=5, pady=10)
-    frame_grande.grid_rowconfigure(1, weight=1)
-    frame_grande.grid_columnconfigure(0, weight=1)
+        nombreProceso = tk.Label(zona1, text= self.nombre)
+        nombreProceso.grid(row=0, column=0, padx=5, pady=5)
+        zona1.grid_rowconfigure(0, weight=1)
+        zona1.grid_columnconfigure(0, weight=1)
 
-    zona1 = tk.Frame(marco, bg="yellow", borderwidth=1, relief="solid")
-    zona1.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
-    marco.grid_rowconfigure(0, weight=1)
-    marco.grid_columnconfigure(0, weight=1)
+        descripcionProceso = tk.Label(zona1, text= self.descripcion)
+        descripcionProceso.grid(row=1, column=0, padx=5, pady=5)
+        zona1.grid_rowconfigure(1, weight=1)
+        zona1.grid_columnconfigure(0, weight=1)
 
-    zona2 = tk.Frame(marco, bg="orange", borderwidth=1, relief="solid")
-    zona2.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
-    marco.grid_rowconfigure(1, weight=1)
-    marco.grid_columnconfigure(0, weight=1)
-
-    formElement = FieldFrame("Preguntas", ["Edad", "Pizza de piña?", "Secso"], "Entradas", ["Edad", "Pizza de piña?", "Secso"], None, zona2)
-
-    pass
+        formElement = FieldFrame("Preguntas", self.criterios, "Entradas", self.criterios, None, zona2)
 
 
 def genComprarVuelo():
@@ -214,6 +254,11 @@ def makePopUp():
 
 
 
-genMainMenu()
+#ventanaInicial = VentanaInicial()
+#ventanaInicial.generar()
+
+mainMenu = MainMenu()
+mainMenu.generar()
+
 
 App.mainloop()
