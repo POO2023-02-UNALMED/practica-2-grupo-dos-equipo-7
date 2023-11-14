dd9fa5e7be124ac36d39aabbc0cf9bf1ebb45125
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-11-14 09:09:23 -0500

    a

---
+++ src/gestorAplicacion/baseDatos/Serializador.py
@@ -0,0 +1,12 @@
+from pickle import dump
+
+from src.gestorAplicacion.Aerolinea.Boleto import Boleto
+
+
+class Serializador:
+    def __init__(self) :
+        pass
+
+    def SerializarVuelo(self,boleto : Boleto):
+        return
+
