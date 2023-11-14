5c8e74d61b3292b3c77c35ef30764eda42f49abb
AlejaMuGo <141966575+AlejaMuGo@users.noreply.github.com>
2023-11-14 20:53:45 +0000

    fix imports

--- src/baseDatos/Serializador.py
+++ src/baseDatos/Serializador.py
@@ -1,7 +1,23 @@
 from pickle import dump
 
-from gestorAplicacion.Aerolinia.Boleto import Boleto
+# Imports correctos
+from gestorAplicacion.Aerolinea.Asiento import Asiento
+from gestorAplicacion.Aerolinea.Boleto import Boleto
+from gestorAplicacion.Aerolinea.Maleta import Maleta
+from gestorAplicacion.Aerolinea.RestriccionesMaleta import RestriccionesMaleta
+from gestorAplicacion.Aerolinea.ServiciosEspeciales import ServiciosEspeciales
+from gestorAplicacion.Aerolinea.Vuelo import Vuelo
 
+from gestorAplicacion.Cuenta.Usuario import Usuario
+
+from gestorAplicacion.Descuentos.Descuento import Descuento
+from gestorAplicacion.Descuentos.descuentoMaleta import descuentoMaleta
+from gestorAplicacion.Descuentos.descuentoVuelo import descuentoVuelo
+from gestorAplicacion.Descuentos.upgradeAsiento import upgradeAsiento
+
+from gestorAplicacion.Mascotas.Animal import Animal
+from gestorAplicacion.Mascotas.Perro import Perro
+from gestorAplicacion.Mascotas.Gato import Gato
 
 class Serializador:
     def __init__(self) :
